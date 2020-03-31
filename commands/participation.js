8c3721f51aab7957394f718996969610cc22ebcb
lgarcin <laurent.garcin@gmail.com>
2020-03-31 16:10:28 +0200

    Update participation.js

--- commands/participation.js
+++ commands/participation.js
@@ -8,8 +8,6 @@ module.exports = {
 			? message.guild.members
 			: message.mentions.members
 		).fetch()).filter(member => !member.user.bot);
-		console.log(message.mentions.channels);
-		console.log(message.guild.channels.cache);
 		const channels = (message.mentions.channels.size == 0
 			? message.guild.channels.cache
 			: message.mentions.channels
@@ -19,28 +17,14 @@ module.exports = {
 			members.filter(member => !member.user.bot).map(member => [member, 0]),
 		);
 
-		Promise.all(
-			channels.map(channel =>
-				channel.fetchMessages({ limit: 100 }).catch(() => {
-					return 'rejected';
-				}),
-			),
-		).then(results => {
-			results
-				.filter(result => result !== 'rejected')
-				.forEach(messages =>
-					messages
-						.filter(msg => part.has(msg.member))
-						.forEach(msg =>
-							part.set(msg.member, part.get(msg.member) + 1),
-						),
-				);
-			const response = Array.from(part)
-				.sort(([, n1], [, n2]) => n2 - n1)
-				.map(([member, nb_messages]) => member + ' ' + nb_messages)
-				.join('\n');
-			message.reply(response);
+		const response = Array.from(
+			channels
+				.flatMap(channel => channel.cache)
+				.reduce((acc, msg) => acc.set(msg.member, acc.get(msg.member) + 1), part))
+			.sort(([, n1], [, n2]) => n2 - n1)
+			.map(([member, nb_messages]) => member + ' ' + nb_messages)
+			.join('\n');
 
-		});
+		message.reply(response);
 	},
 };
