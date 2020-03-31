aa8a59e81b637bf939028b809ba2a6588affaa97
lgarcin <laurent.garcin@gmail.com>
2020-03-31 21:54:03 +0200

    Update participation.js

--- commands/participation.js
+++ commands/participation.js
@@ -20,7 +20,7 @@ module.exports = {
 		const results = await Promise.allSettled(channels.map(channel => channel.messages.fetch()));
 
 		console.log(results.filter(result => result.status === 'fulfilled')
-			.flatMap(result => result.value));
+			.flatMap(result => result.value.values()));
 
 		const response = Array.from(results.filter(result => result.status === 'fulfilled')
 			.flatMap(result => result.value)
