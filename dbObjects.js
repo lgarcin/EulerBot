c451c129b0313df630f3a969ccf929a72435e7a5
lgarcin <laurent.garcin@gmail.com>
2021-06-07 21:46:22 +0200

    Update dbObjects.js

--- dbObjects.js
+++ dbObjects.js
@@ -1,13 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    }
-});
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 const Users = sequelize.import('models/Users');
 const Replies = sequelize.import('models/Replies');
